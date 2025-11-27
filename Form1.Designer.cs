1e777af3ea6b0c5862294e6782ecbddaad1a62b8
mozartist <153170138+mozartist@users.noreply.github.com>
2025-11-27 19:48:36 +0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,182 @@
+﻿namespace sayi_bilmece61
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        private void InitializeComponent()
+        {
+            this.lblKalanHakEtiket = new System.Windows.Forms.Label();
+            this.lblKalanHak = new System.Windows.Forms.Label();
+            this.lblDurum = new System.Windows.Forms.Label();
+            this.lblSonucOzet = new System.Windows.Forms.Label();
+            this.lblSonucDetay = new System.Windows.Forms.Label();
+            this.txtTahmin = new System.Windows.Forms.TextBox();
+            this.btnTahmin = new System.Windows.Forms.Button();
+            this.btnBaslat = new System.Windows.Forms.Button();
+            this.btnYenidenOyna = new System.Windows.Forms.Button();
+            this.lstGecmis = new System.Windows.Forms.ListBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblKalanHakEtiket
+            // 
+            this.lblKalanHakEtiket.AutoSize = true;
+            this.lblKalanHakEtiket.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lblKalanHakEtiket.Location = new System.Drawing.Point(40, 25);
+            this.lblKalanHakEtiket.Name = "lblKalanHakEtiket";
+            this.lblKalanHakEtiket.Size = new System.Drawing.Size(90, 23);
+            this.lblKalanHakEtiket.TabIndex = 0;
+            this.lblKalanHakEtiket.Text = "Kalan Hak:";
+            // 
+            // lblKalanHak
+            // 
+            this.lblKalanHak.AutoSize = true;
+            this.lblKalanHak.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.lblKalanHak.Location = new System.Drawing.Point(136, 25);
+            this.lblKalanHak.Name = "lblKalanHak";
+            this.lblKalanHak.Size = new System.Drawing.Size(30, 23);
+            this.lblKalanHak.TabIndex = 1;
+            this.lblKalanHak.Text = "10";
+            // 
+            // lblDurum
+            // 
+            this.lblDurum.AutoSize = true;
+            this.lblDurum.Location = new System.Drawing.Point(40, 60);
+            this.lblDurum.Name = "lblDurum";
+            this.lblDurum.Size = new System.Drawing.Size(125, 20);
+            this.lblDurum.TabIndex = 2;
+            this.lblDurum.Text = "Tahmininizi Girin:";
+            // 
+            // lblSonucOzet
+            // 
+            this.lblSonucOzet.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblSonucOzet.Location = new System.Drawing.Point(40, 85);
+            this.lblSonucOzet.Name = "lblSonucOzet";
+            this.lblSonucOzet.Size = new System.Drawing.Size(300, 30);
+            this.lblSonucOzet.TabIndex = 3;
+            this.lblSonucOzet.Text = "Sonuç: +0, -0";
+            this.lblSonucOzet.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // lblSonucDetay
+            // 
+            this.lblSonucDetay.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblSonucDetay.Location = new System.Drawing.Point(40, 125);
+            this.lblSonucDetay.Name = "lblSonucDetay";
+            this.lblSonucDetay.Size = new System.Drawing.Size(300, 110);
+            this.lblSonucDetay.TabIndex = 4;
+            this.lblSonucDetay.Text = "Detaylar burada görünecek...";
+            this.lblSonucDetay.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // txtTahmin
+            // 
+            this.txtTahmin.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.txtTahmin.Location = new System.Drawing.Point(40, 250);
+            this.txtTahmin.MaxLength = 4;
+            this.txtTahmin.Name = "txtTahmin";
+            this.txtTahmin.Size = new System.Drawing.Size(160, 34);
+            this.txtTahmin.TabIndex = 5;
+            this.txtTahmin.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtTahmin_KeyPress);
+            // 
+            // btnTahmin
+            // 
+            this.btnTahmin.Location = new System.Drawing.Point(220, 248);
+            this.btnTahmin.Name = "btnTahmin";
+            this.btnTahmin.Size = new System.Drawing.Size(120, 40);
+            this.btnTahmin.TabIndex = 6;
+            this.btnTahmin.Text = "Tahmin Et";
+            this.btnTahmin.UseVisualStyleBackColor = true;
+            this.btnTahmin.Click += new System.EventHandler(this.btnTahmin_Click);
+            // 
+            // btnBaslat
+            // 
+            this.btnBaslat.Location = new System.Drawing.Point(40, 300); // Sola kaydırdım
+            this.btnBaslat.Name = "btnBaslat";
+            this.btnBaslat.Size = new System.Drawing.Size(140, 40);
+            this.btnBaslat.TabIndex = 7;
+            this.btnBaslat.Text = "Başlat";
+            this.btnBaslat.UseVisualStyleBackColor = true;
+            this.btnBaslat.Click += new System.EventHandler(this.btnBaslat_Click);
+            // 
+            // btnYenidenOyna
+            // 
+            this.btnYenidenOyna.BackColor = System.Drawing.Color.IndianRed; // Kırmızımsı renk
+            this.btnYenidenOyna.ForeColor = System.Drawing.Color.White;
+            this.btnYenidenOyna.Location = new System.Drawing.Point(200, 300); // Başlat'ın sağına
+            this.btnYenidenOyna.Name = "btnYenidenOyna";
+            this.btnYenidenOyna.Size = new System.Drawing.Size(140, 40);
+            this.btnYenidenOyna.TabIndex = 10;
+            this.btnYenidenOyna.Text = "Yeniden Oyna";
+            this.btnYenidenOyna.UseVisualStyleBackColor = false;
+            this.btnYenidenOyna.Click += new System.EventHandler(this.btnBaslat_Click); // Aynı işi yapacak
+            // 
+            // lstGecmis
+            // 
+            this.lstGecmis.FormattingEnabled = true;
+            this.lstGecmis.ItemHeight = 20;
+            this.lstGecmis.Location = new System.Drawing.Point(40, 360);
+            this.lstGecmis.Name = "lstGecmis";
+            this.lstGecmis.Size = new System.Drawing.Size(300, 144);
+            this.lstGecmis.TabIndex = 8;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(380, 25);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.Size = new System.Drawing.Size(400, 480);
+            this.dataGridView1.TabIndex = 9;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(820, 530);
+            this.Controls.Add(this.btnYenidenOyna); 
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.lstGecmis);
+            this.Controls.Add(this.btnBaslat);
+            this.Controls.Add(this.btnTahmin);
+            this.Controls.Add(this.txtTahmin);
+            this.Controls.Add(this.lblSonucDetay);
+            this.Controls.Add(this.lblSonucOzet);
+            this.Controls.Add(this.lblDurum);
+            this.Controls.Add(this.lblKalanHak);
+            this.Controls.Add(this.lblKalanHakEtiket);
+            this.Name = "Form1";
+            this.Text = "Sayı Tahmin Etme";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblKalanHakEtiket;
+        private System.Windows.Forms.Label lblKalanHak;
+        private System.Windows.Forms.Label lblDurum;
+        private System.Windows.Forms.Label lblSonucOzet;
+        private System.Windows.Forms.Label lblSonucDetay;
+        private System.Windows.Forms.TextBox txtTahmin;
+        private System.Windows.Forms.Button btnTahmin;
+        private System.Windows.Forms.Button btnBaslat;
+        private System.Windows.Forms.Button btnYenidenOyna;
+        private System.Windows.Forms.ListBox lstGecmis;
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,194 @@
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace sayi_bilmece61
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //SQL
+        SqlConnection baglanti = new SqlConnection("Data Source=.;Initial Catalog=SayiTahminDB;Integrated Security=True");
+
+        int randomSayi;
+        int hak;
+        string hedefSayiString = "";
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            OyunBittiModu();
+            GecmisiListele();
+        }
+
+        private void btnBaslat_Click(object sender, EventArgs e)
+        {
+            Random rnd = new Random();
+            while (true)
+            {
+                randomSayi = rnd.Next(1000, 10000);
+                hedefSayiString = randomSayi.ToString();
+
+                // Rakamlarý birbirinden farklý mý kontrolü
+                if (hedefSayiString[0] != hedefSayiString[1] &&
+                    hedefSayiString[0] != hedefSayiString[2] &&
+                    hedefSayiString[0] != hedefSayiString[3] &&
+                    hedefSayiString[1] != hedefSayiString[2] &&
+                    hedefSayiString[1] != hedefSayiString[3] &&
+                    hedefSayiString[2] != hedefSayiString[3])
+                {
+                    break;
+                }
+            }
+
+            hak = 10;
+            lblKalanHak.Text = hak.ToString();
+            lblDurum.Text = "Tahmininizi Girin";
+            lblDurum.ForeColor = Color.Black;
+            lblSonucOzet.Text = "Sonuç: +0, -0";
+            lblSonucDetay.Text = "Yeni oyun baþladý! Baþarýlar...";
+            lstGecmis.Items.Clear();
+            txtTahmin.Clear();
+
+            txtTahmin.Enabled = true;
+            btnTahmin.Enabled = true;
+
+            btnBaslat.Enabled = false;
+            btnYenidenOyna.Enabled = true; 
+
+            txtTahmin.Focus();
+        }
+
+        private void btnTahmin_Click(object sender, EventArgs e)
+        {
+            lblDurum.Text = "";
+            lblDurum.ForeColor = Color.Black;
+
+            string tahmin = txtTahmin.Text;
+
+            if (string.IsNullOrWhiteSpace(tahmin) || tahmin.Length != 4)
+            {
+                lblDurum.Text = "Lütfen 4 haneli sayý girin!";
+                lblDurum.ForeColor = Color.Red;
+                return;
+            }
+
+            if (tahmin[0] == tahmin[1] || tahmin[0] == tahmin[2] || tahmin[0] == tahmin[3] ||
+                tahmin[1] == tahmin[2] || tahmin[1] == tahmin[3] || tahmin[2] == tahmin[3])
+            {
+                lblDurum.Text = "Rakamlar farklý olmalý!";
+                lblDurum.ForeColor = Color.Red;
+                txtTahmin.Clear();
+                return;
+            }
+
+            string detayliRapor = "";
+            int dogruYer = 0;
+
+            for (int i = 0; i < 4; i++)
+            {
+                char girilenRakam = tahmin[i];
+                char hedefRakam = hedefSayiString[i];
+
+                if (girilenRakam == hedefRakam)
+                {
+                    dogruYer++;
+                    detayliRapor += (i + 1) + ". Sayý: DOÐRU\n";
+                }
+                else if (hedefSayiString.Contains(girilenRakam))
+                {
+                    detayliRapor += (i + 1) + ". Sayý: Rakam doðru ama YERÝ YANLIÞ\n";
+                }
+                else
+                {
+                    detayliRapor += (i + 1) + ". Sayý: YANLIÞ\n";
+                }
+            }
+
+            hak--;
+            lblKalanHak.Text = hak.ToString();
+
+            lblSonucOzet.Text = "Tahmin Analizi:";
+            lblSonucDetay.Text = detayliRapor;
+            lstGecmis.Items.Add(tahmin + " -> (Analiz yapýldý)");
+
+            if (dogruYer == 4)
+            {
+                lblSonucDetay.Text = "TEBRÝKLER! TÜM SAYILAR DOÐRU!";
+                MessageBox.Show("Tebrikler Kazandýnýz! Sayý: " + hedefSayiString);
+                VeritabaniKayit(hedefSayiString, "Kazandý", hak);
+                OyunBittiModu();
+            }
+            else if (hak == 0)
+            {
+                MessageBox.Show("Oyun Bitti. Doðru Sayý: " + hedefSayiString);
+                VeritabaniKayit(hedefSayiString, "Kaybetti", 0);
+                OyunBittiModu();
+            }
+
+            txtTahmin.Clear();
+            txtTahmin.Focus();
+        }
+
+        private void txtTahmin_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsDigit(e.KeyChar) && e.KeyChar != (char)8)
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void OyunBittiModu()
+        {
+            txtTahmin.Enabled = false;
+            btnTahmin.Enabled = false;
+
+            btnBaslat.Enabled = true;
+            btnYenidenOyna.Enabled = true;
+        }
+
+        private void VeritabaniKayit(string hedef, string durum, int kalan)
+        {
+            try
+            {
+                if (baglanti.State == ConnectionState.Closed) baglanti.Open();
+                SqlCommand komut = new SqlCommand("INSERT INTO OyunGecmisi (HedefSayi, Durum, KalanHak, Tarih) VALUES (@p1, @p2, @p3, @p4)", baglanti);
+                komut.Parameters.AddWithValue("@p1", hedef);
+                komut.Parameters.AddWithValue("@p2", durum);
+                komut.Parameters.AddWithValue("@p3", kalan);
+                komut.Parameters.AddWithValue("@p4", DateTime.Now);
+                komut.ExecuteNonQuery();
+                baglanti.Close();
+                GecmisiListele();
+            }
+            catch (Exception ex)
+            {
+                if (baglanti.State == ConnectionState.Open) baglanti.Close();
+            }
+        }
+
+        private void GecmisiListele()
+        {
+            try
+            {
+                if (baglanti.State == ConnectionState.Closed) baglanti.Open();
+                SqlDataAdapter da = new SqlDataAdapter("SELECT * FROM OyunGecmisi ORDER BY Tarih DESC", baglanti);
+                DataTable dt = new DataTable();
+                da.Fill(dt);
+                dataGridView1.DataSource = dt;
+                baglanti.Close();
+            }
+            catch
+            {
+                if (baglanti.State == ConnectionState.Open) baglanti.Close();
+            }
+        }
+    }
+}
